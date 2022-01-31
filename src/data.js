2202ce55ad66af948cafbb422ec6c10e44a228e7
bomdiavei <bomdiavei@hotmail.com>
2022-01-30 21:20:01 -0400

    Shopping Categories

--- src/data.js
+++ src/data.js
@@ -2,6 +2,19 @@ import sliderImageOne from './assets/images/1.png'
 import sliderImageTwo from './assets/images/2.png'
 import sliderImageThree from './assets/images/3.png'
 
+import catImageOne from './assets/images/cat1.jpeg'
+import catImageTwo from './assets/images/cat2.jpeg'
+import catImageThree from './assets/images/cat3.jpeg'
+
+import popImageOne from './assets/images/pop1.png'
+import popImageTwo from './assets/images/pop2.png'
+import popImageThree from './assets/images/pop3.png'
+import popImageFour from './assets/images/pop4.png'
+import popImageFive from './assets/images/pop5.png'
+import popImageSix from './assets/images/pop6.png'
+import popImageSeven from './assets/images/pop7.png'
+import popImageEight from './assets/images/pop8.png'
+
 export const sliderItems = [
     {
       id: 1,
@@ -29,17 +42,17 @@ export const sliderItems = [
   export const categories = [
     {
       id: 1,
-      img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      img: catImageOne,
       title: "SHIRT STYLE!",
     },
     {
       id: 2,
-      img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      img: catImageTwo,
       title: "LOUNGEWEAR LOVE",
     },
     {
       id: 3,
-      img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      img: catImageThree,
       title: "LIGHT JACKETS",
     },
   ];
@@ -47,34 +60,34 @@ export const sliderItems = [
   export const popularProducts = [
     {
       id:1,
-      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+      img: popImageOne,
     },
     {
       id:2,
-      img:"https://cdn.shopify.com/s/files/1/0101/4832/products/Angela_Natural_Tee.png?v=1606780388",
+      img: popImageTwo,
     },
     {
       id:3,
-      img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+      img: popImageThree,
     },
     {
       id:4,
-      img:"https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+      img: popImageFour,
     },
     {
       id:5,
-      img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+      img: popImageFive,
     },
     {
       id:6,
-      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+      img: popImageSix,
     },
     {
       id:7,
-      img:"https://www.vintageindustries.nl/download_front/qympzk1762/2217_Arrow_Jacket_Forest.png",
+      img: popImageSeven,
     },
     {
       id:8,
-      img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+      img: popImageEight,
     },
   ]
